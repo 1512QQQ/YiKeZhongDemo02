000c1ea96d7c82931b103f3fd4786e78c1a26191
PremierAmour <2416265825@qq.com>
2018-06-09 10:03:56 +0800

    布局

--- app/src/main/java/com/example/administrator/yikezhong/slass/leftactivity/Head_login.java
+++ app/src/main/java/com/example/administrator/yikezhong/slass/leftactivity/Head_login.java
@@ -10,28 +10,28 @@ import android.widget.TextView;
 import com.example.administrator.yikezhong.MainActivity;
 import com.example.administrator.yikezhong.R;
 
-public class HeadPic extends AppCompatActivity {
+public class Head_login extends AppCompatActivity {
 
-    private TextView it_longin;
-    private ImageView back01;
+    private ImageView back02;
+    private TextView from;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_head_pic);
-        it_longin = findViewById(R.id.it_longin);
-        back01 = findViewById(R.id.back01);
-        it_longin.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_head_login);
+        back02 = findViewById(R.id.back02);
+        from = findViewById(R.id.from);
+        back02.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HeadPic.this, Head_login.class);
+                Intent intent = new Intent(Head_login.this, HeadPic.class);
                 startActivity(intent);
             }
         });
-        back01.setOnClickListener(new View.OnClickListener() {
+        from.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent2 = new Intent(HeadPic.this, MainActivity.class);
+                Intent intent2 = new Intent(Head_login.this, MainActivity.class);
                 startActivity(intent2);
             }
         });
